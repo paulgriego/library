86173a8f1c8f85191b7f70b4ab95e1ff6f407485
Paul Griego <paulgriego029@gmail.com>
2022-04-26 13:53:06 -0600

    Added function to save input from form and add card to page

--- library.js
+++ library.js
@@ -3,24 +3,41 @@ function openForm() {
 }
 function closeForm() {
   document.getElementById("formpop").style.display = "none";
+
 }
-
-
-
-
 
 function book(title, author, pages,read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
-
 }
+ 
 
  function addBooktoLibrary() {
+  let title =document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  myLibrary.push(new book(title,author,pages))
 }
 
-const HarryPotter = new book("Harry Potter", "JK Rowlings", "393", "Yes");
+function addBooktoPage() {
+    const container = document.querySelector('#book');
+    const content = document.createElement('div');
+    content.classList.add('card');
+    const para = document.createElement("p");
+    const node = document.createTextNode("test");
+    para.appendChild(node);
+    content.appendChild(para);
+    container.appendChild(content);
+
+}
 
 
-let myLibrary = [HarryPotter];
+
+let myLibrary = [];
+
+
+
+// Object.values(myLibrary[1])
+
